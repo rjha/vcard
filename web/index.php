7e4a64cfadb236d9b550b7c60e99b127a8672e6e
Rajeev Jha <jha.rajeev@gmail.com>
2021-04-25 12:13:38 +0530

    vcard page changes
    - add remove link to entries
    - add total trash count
    - add background color on remove

--- web/index.php
+++ web/index.php
@@ -28,47 +28,74 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css">
- 
+    
+    <style>
+
+        .strike {
+            background-color: coral;
+        }
+
+    </style>
+
 </head>
 
 <body>
 
 
-<div class="container">
+<div class="container" id="container">
     <header class="sticky">
         <h1> visiting cards database </h1>
     </header>
 
     <div id="cards">
-        <div class="row" v-for="card in cards">
+        <div class="row" v-for="card in cards" v-bind:class="{strike: card.hide}">
             <div class="col-sm-4"> {{card.name}} </div>  
             <div class="col-sm-4"> {{card.email}} </div>
+            <div class="col-sm-4"> <a href="#" v-on:click="removeEmail(card)">Remove</a> </div>
         </div>
     </div>
 
     
 
     <footer class="sticky">
-        <a href="/index.php?page=<?php echo $previousPage; ?>">&lt;&nbsp;previous</a>
-        &nbsp;&nbsp;
-        <a href="/index.php?page=<?php echo $nextPage; ?>">next&nbsp;&gt;</a>
+        <div class="row">
+            <div class="col-sm-6"> 
+                <a href="/index.php?page=<?php echo $previousPage; ?>">&lt;&nbsp;previous</a>
+                &nbsp;&nbsp;
+                <a href="/index.php?page=<?php echo $nextPage; ?>">next&nbsp;&gt;</a>
+            </div>
+            <div class="col-sm-6"> 
+                <span> Removed {{trash.length}}</span>
+            </div>
+        </div>
+
+        
     </footer>
 
   </div>
 
   <script src="https://unpkg.com/vue@next"></script>
   <script>
-     var gparams = <?php echo json_encode($gparams, JSON_PRETTY_PRINT); ?>;
-
+    
+    var gparams = <?php echo json_encode($gparams, JSON_PRETTY_PRINT); ?>;
+    
     const Contact = {
         data() {
             return {
-                cards: gparams.cards
+                cards: gparams.cards,
+                trash: []
+            }
+        },
+        methods: {
+            removeEmail(card) {
+                this.trash.push(card.email);
+                card.hide = true;
+                console.log("clicked %s, trash %O", card.email, this.trash.length);
             }
         }
     };
 
-    Vue.createApp(Contact).mount('#cards')
+    Vue.createApp(Contact).mount('#container')
     </script>
 
 </body>
