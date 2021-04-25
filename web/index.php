b243ab32c0d24ab87cbb4740b4c961b1b7d4567f
Rajeev Jha <jha.rajeev@gmail.com>
2021-04-25 13:05:48 +0530

    vcard page style fixes and restore link
    - add implementation of trash and restore functions
    - add message div for server messages
    - add trash row style

--- web/index.php
+++ web/index.php
@@ -31,8 +31,16 @@
     
     <style>
 
+        header {
+            text-align: center;
+        }
+
+        #message {
+            margin-bottom: 30px;
+            background: azure;
+        }
         .strike {
-            background-color: coral;
+            background-color: bisque;
         }
 
     </style>
@@ -47,26 +55,42 @@
         <h1> visiting cards database </h1>
     </header>
 
+    <div id="message"> 
+        <div class="row">
+            <span> &nbsp; </span>
+        </div>
+    </div>
+
     <div id="cards">
-        <div class="row" v-for="card in cards" v-bind:class="{strike: card.hide}">
+        
+        <div class="row" v-for="card in cards" v-bind:class="{strike: card.trash}">
             <div class="col-sm-4"> {{card.name}} </div>  
             <div class="col-sm-4"> {{card.email}} </div>
-            <div class="col-sm-4"> <a href="#" v-on:click="removeEmail(card)">Remove</a> </div>
+            <div class="col-sm-4"> 
+                <span v-if="!card.trash"><a href="#" v-on:click="trashCard($event, card)">Trash</a> </span>
+                <span v-if="card.trash"><a href="#" v-on:click="restoreCard($event, card)">Restore</a> </span>
+            </div>
         </div>
     </div>
 
     
 
     <footer class="sticky">
+        
         <div class="row">
-            <div class="col-sm-6"> 
+            <div class="col-sm-4"> 
                 <a href="/index.php?page=<?php echo $previousPage; ?>">&lt;&nbsp;previous</a>
                 &nbsp;&nbsp;
                 <a href="/index.php?page=<?php echo $nextPage; ?>">next&nbsp;&gt;</a>
             </div>
             <div class="col-sm-6"> 
-                <span> Removed {{trash.length}}</span>
+                <span> Trash {{trash.length}}</span>
             </div>
+
+            <div class="col-sm-2"> 
+                <a href="#">submit</a>
+            </div>
+
         </div>
 
         
@@ -87,11 +111,41 @@
             }
         },
         methods: {
-            removeEmail(card) {
-                this.trash.push(card.email);
-                card.hide = true;
-                console.log("clicked %s, trash %O", card.email, this.trash.length);
+            trashCard(event, card) {
+                // This is to ensure that link
+                // stays in the same place 
+                event.preventDefault();
+                this.trash.push(card);
+                card.trash = true;
+                console.log("clicked %s, trash size %O", card.email, this.trash.length);
+                
+            },
+
+            restoreCard(event, card) {
+
+                console.log("restore card -> email %s", card.email);
+                let index =0;
+                let found = true;
+                let i = 0;
+                event.preventDefault();
+                card.trash = false;
+                for(i =0; i < this.trash.length; i++) {
+                    if(this.trash[i].email == card.email) {
+                        console.log("card.email matched @index: %s, trash card %s", i, this.trash[i].email);
+                        index = i;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(found) {
+                    let restored = this.trash.splice(index, 1);
+                    console.log("restore from trash: %O", restored);
+                }
+
+
             }
+
         }
     };
 
